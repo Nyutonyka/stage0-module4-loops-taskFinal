04141aa702d744c30ad431decc3c940a21b0739e
nyuto <nuytonyka@gmail.com>
2023-02-07 21:14:30 +0200

    Solved

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,7 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
-
+        int powerNumber = 1;
+        for (int i = 0; i < power; i++){
+            powerNumber = powerNumber * numberToPrint;
+        }
+        System.out.println(powerNumber);
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,21 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int j = cathetusLength; j > 1; j--) {
+                if (i == j || i > j) {
+                    System.out.print(j);
+                    continue;
+                }
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= cathetusLength; j++) {
+                if (i == j || i > j) {
+                    System.out.print(j);
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
